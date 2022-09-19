e252bb9d8d06b32f4297a51612af46caf32c6ef5
romeo <romeomeshack34@gmail.com>
2022-09-19 04:51:40 +0100

    SEO component

---
+++ src/components/SEO.tsx
@@ -0,0 +1,48 @@
+import { chdir } from "process"
+import React from "react"
+import { useSiteMetaData } from "../hooks/useSiteMetaData"
+
+type SEOType = {
+  title?: string
+  description?: string
+  pathname?: string
+  children?: JSX.Element
+}
+
+export default function SEO({
+  title,
+  description,
+  pathname,
+  children,
+}: SEOType) {
+  const {
+    title: defaultTitle,
+    description: defaultDescription,
+    siteUrl,
+    author,
+  } = useSiteMetaData()
+
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    url: `${siteUrl}${pathname}`,
+  }
+
+  return (
+    <>
+      <title>{seo.title}</title>
+      <meta name="description" content={seo.description} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:url" content={seo.url} />
+      <meta name="twitter:description" content={seo.description} />
+      {/* <meta name="twitter:image" content={seo.image} /> */}
+      {/* <meta name="twitter:creator" content={seo.twitterUsername} /> */}
+      <meta name="og:title" content={seo.title} />
+      <meta name="og:description" content={seo.description} />
+      <meta name="og:type" content="website" />
+      {/* <meta name="og:image" content="" /> */}
+      {children}
+    </>
+  )
+}
