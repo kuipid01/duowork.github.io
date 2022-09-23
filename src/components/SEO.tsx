7660c80a19e76ca0d1d9e645bcda16470743775c
romeo <romeomeshack34@gmail.com>
2022-09-23 14:51:41 +0100

    Modified

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -41,8 +41,34 @@ export default function SEO({
       <meta name="og:title" content={seo.title} />
       <meta name="og:description" content={seo.description} />
       <meta name="og:type" content="website" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+
+      {/* Favicon */}
+      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      <meta name="theme-color" content="#ffffff" />
+
       {/* <meta name="og:image" content="" /> */}
-      <script src="https://kit.fontawesome.com/f65742b227.js" crossOrigin="anonymous"></script>
+      <script
+        src="https://kit.fontawesome.com/f65742b227.js"
+        crossOrigin="anonymous"
+      ></script>
+
+      {/* Favicon */}
+      <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+      <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+      <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/manifest.json"/>
+
       {children}
     </>
   )
