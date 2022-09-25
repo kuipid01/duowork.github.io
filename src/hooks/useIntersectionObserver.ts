2f648a845475e97158e3862ffdcb595ac0c28616
romeo <romeomeshack34@gmail.com>
2022-09-25 18:23:03 +0100

    added Intersection observer hook

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -1,6 +1,6 @@
 type callbackType = () => void
 
-function useIntersectionObserver(callback: callbackType, options: undefined) {
+export function useIntersectionObserver(callback: callbackType, options: undefined) {
   const observer = new IntersectionObserver(callback, options)
   return observer
 }
