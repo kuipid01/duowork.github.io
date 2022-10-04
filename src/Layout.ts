fb3ec4e1ee51ab4ae9511186695848e74c99246d
romeo <romeomeshack34@gmail.com>
2022-10-04 13:23:08 +0100

    New component

---
+++ src/Layout.ts
@@ -0,0 +1,7 @@
+import React, { ReactHTMLElement } from 'react'
+
+export default function Layout({chidren}: any) {
+  // return (
+  //   <main>{children}</main>
+  // )
+}
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <section>
+      {/*
+        Form fileds
+        ..........
+        FullName Company name(optional)
+        Email address 
+        phone (optional)
+        Survey -- How they found us
+        Pre-filled selection -- Type of project
+         - Product design
+         - Build a product
+         - Automate task
+        Text-area -- Project description
+        Budget -- 
+       */}
+    </section>
   )
 }
