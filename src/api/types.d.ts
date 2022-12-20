469a6c8842aa84912a46d6e66657769681a4e797
romeo <romeomeshack34@gmail.com>
2022-12-20 16:05:12 +0100

    Removed google API

--- src/api/types.d.ts
+++
@@ -1,9 +0,0 @@
-export type RequestObjDataType = {
-    date: string
-    fullName: string
-    email: string
-    contactSurvey: string
-    serviceType: string
-    serviceDescription: string
-    budget?: string
-}
