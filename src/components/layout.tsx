b7de8cd8337ded78ce88b84aa0bbe3841f0da755
romeo <romeomeshack34@gmail.com>
2023-01-02 20:55:41 +0100

    Modified

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from "react"
 import { Link } from "gatsby"
 
-import SEO from "./SEO"
+import Button from "./Button"
 
 import { ToastContainer } from "react-toastify"
 import customCursor from "../../lib/customCursor"
@@ -11,6 +11,53 @@ import logo from "../assets/logos/logo.png"
 import navBurgerBar from "../assets/icons/navbar-burger-solid.svg"
 import navXmark from "../assets/icons/navbar-xmark-solid.svg"
 
+const DesktopNavigation = (): React.ReactElement | null => {
+  // Check page location and appy right styling
+  const location = typeof window !== "undefined" ? window.location : undefined
+  const homepage = location?.pathname === "/";
+
+  return (
+    <>
+      <nav
+        className={`landing-page-nav text-white ml-10 mr-10 mt-5 ${homepage? 'hidden':'flex flex-row justify-between'}`}
+      >
+        <div
+          id="logo-container"
+          className="w-11 h-11 bg-white rounded-full flex flex-row justify-center items-center"
+        >
+          {/* <GatsbyImage alt="Website logo, duowork" image={logo} id="logo" /> */}
+          <Link to="/">
+            <img src={logo} alt="Website logo, duowork" id="logo" />
+          </Link>
+        </div>
+
+        <ul id="nav-items" className={`hidden md:flex flex-row justify-between ${homepage? 'text-white':'text-black'}`}>
+          <li className="nav-item-link">
+            <Link to="/#landing-page-home">Home</Link>
+          </li>
+          <li className="nav-item-link">
+            <Link to="/#our-work">Our work</Link>
+          </li>
+          <li className="nav-item-link">
+            <Link to="/#our-services">Service</Link>
+          </li>
+          {/* <li className="nav-item-link">
+            <Link to="/blog">Blog</Link>
+          </li> */}
+          <li id="cta-nav-item-contact" className="nav-item-link">
+            <Button
+              value="Contact Us"
+              btnClass="cta-btn nav-contact-cta text-white"
+              isLink={true}
+              linkTo="/#contact-duowork"
+            />
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
 export default function Layout({ children }: any) {
 
   // Navigation ref
@@ -19,35 +66,34 @@ export default function Layout({ children }: any) {
   const mobileNav = useRef<HTMLElement>(null)
 
   // Custom cursor ref
-  const cursorDotRef = useRef<HTMLImageElement>(null);
-  const cursorDotOutlineRef = useRef<HTMLImageElement>(null);
+  const cursorDotRef = useRef<HTMLImageElement>(null)
+  const cursorDotOutlineRef = useRef<HTMLImageElement>(null)
 
   useEffect((): any => {
-    let componentIsMounted = true;
+    let componentIsMounted = true
 
     if (componentIsMounted) {
       // Call custom cursor func
-      customCursor(cursorDotRef, cursorDotOutlineRef);
+      customCursor(cursorDotRef, cursorDotOutlineRef)
     }
-  
-    return () => componentIsMounted = false;
+
+    return () => (componentIsMounted = false)
   }, [])
-  
 
   const handleToggle = (e: any) => {
-    e.preventDefault();
-    const mobileNavElem = mobileNav.current;
+    e.preventDefault()
+    const mobileNavElem = mobileNav.current
 
     if (e.target.attributes[0].nodeValue === "navInitOpen") {
       if (mobileNavElem !== null) {
-        mobileNavElem.classList.add("mobile-nav-container");
+        mobileNavElem.classList.add("mobile-nav-container")
 
         if (navInitOpen.current !== null && navInitClose.current !== null) {
           // Hide menu bar
-          navInitOpen.current.style.display = "none";
+          navInitOpen.current.style.display = "none"
 
           // show X mark
-          navInitClose.current.style.display = "block";
+          navInitClose.current.style.display = "block"
         }
       }
     } else if (e.target.attributes[0].nodeValue === "navInitClose") {
@@ -60,10 +106,10 @@ export default function Layout({ children }: any) {
 
           if (navInitOpen.current !== null && navInitClose.current !== null) {
             // Hide X mark
-            navInitClose.current.style.display = "none";
+            navInitClose.current.style.display = "none"
 
-             // Show menu bar
-             navInitOpen.current.style.display = "block";
+            // Show menu bar
+            navInitOpen.current.style.display = "block"
           }
         }
       }
@@ -72,11 +118,19 @@ export default function Layout({ children }: any) {
 
   return (
     <main id="duowork">
+      {/* Desktop navigation */}
+      <DesktopNavigation />
+
       {/* Mobile navigation */}
       <nav id="mobile-nav" className="flex md:!hidden" ref={mobileNav}>
         {/* <div id="movile-nav-container"> */}
         <div id="open-close-icons" className="p-2 rounded">
-          <a id="openMobileNav" href="#" className="block" onClick={(e) => handleToggle(e)}>
+          <a
+            id="openMobileNav"
+            href="#"
+            className="block"
+            onClick={e => handleToggle(e)}
+          >
             <img
               id="navInitOpen"
               src={navBurgerBar}
@@ -86,7 +140,12 @@ export default function Layout({ children }: any) {
             />
           </a>
 
-          <a id="closeMobileNav" href="#" className="block" onClick={(e) => handleToggle(e)}>
+          <a
+            id="closeMobileNav"
+            href="#"
+            className="block"
+            onClick={e => handleToggle(e)}
+          >
             <img
               id="navInitClose"
               src={navXmark}
@@ -108,7 +167,12 @@ export default function Layout({ children }: any) {
             <Link to="#our-work">Our work</Link>
           </li>
           <li className="nav-item-link">
-            <Link to="#contact-duowork" className="h-full flex items-center justify-center">Contact</Link>
+            <Link
+              to="#contact-duowork"
+              className="h-full flex items-center justify-center"
+            >
+              Contact
+            </Link>
           </li>
         </ul>
         {/* </div> */}
@@ -138,26 +202,30 @@ export default function Layout({ children }: any) {
             <h3 className="text-2xl font-semibold">Services</h3>
             <div id="services">
               <p className="items">
-                <Link to="#our-services">Product Design</Link>
+                <Link to="/#our-services">Product Design</Link>
               </p>
               <p className="items">
-                <Link to="#our-services">Mobile development</Link>
+                <Link to="/#our-services">Mobile development</Link>
               </p>
               <p className="items">
-                <Link to="#our-services">Web development</Link>
+                <Link to="/#our-services">Web development</Link>
               </p>
               <p className="items">
-                <Link to="#our-services">Business automation</Link>
+                <Link to="/#our-services">Business automation</Link>
               </p>
               <p className="items">
-                <Link to="#our-services">Software consulting</Link>
+                <Link to="/#our-services">Software consulting</Link>
               </p>
             </div>
           </div>
 
           <div id="contact" className="text-white">
             <h3 className="text-2xl font-semibold">Contact</h3>
-            <p className="items"><a href="mailto:reach.duowork@gmail.com">reach.duowork@gmail.com</a></p>
+            <p className="items">
+              <a href="mailto:reach.duowork@gmail.com">
+                reach.duowork@gmail.com
+              </a>
+            </p>
             {/* <p className="items">+234 801 234 5478</p> */}
             <div
               id="contact-icons"
@@ -212,7 +280,10 @@ export default function Layout({ children }: any) {
 
       {/* Custom cursor */}
       <div className="cursor-dot text-white" ref={cursorDotRef}></div>
-      <div className="cursor-dot-outline text-white" ref={cursorDotOutlineRef}></div>
+      <div
+        className="cursor-dot-outline text-white"
+        ref={cursorDotOutlineRef}
+      ></div>
     </main>
   )
 }
